9e8bbf2986bdb034d038418182feb232d02fbd6b
unknown <vpas@.ld.yandex.ru>
2011-04-24 18:04:20 +0400

    Initial commit

---
+++ app/controllers/searcher_controller.rb
@@ -0,0 +1,8 @@
+class SearcherController < ApplicationController
+  def main
+  end
+
+  def search
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TyaniTolkay::Application.config.secret_token = 'd7d9a5ccb7a3d37b9561b6c51e43ad9695bbb24ae638bf3813b1f13e262d398016175c189526e99b9e552413edb5ac0d84bbf5c907055b0ca4658f406884b7cc'
